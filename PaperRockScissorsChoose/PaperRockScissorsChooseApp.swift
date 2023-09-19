351ecaee300abe4ca5ab175d33d6545712c6eff8
Danny Tsang <danny.tsang@me.com>
2023-09-19 14:34:19 -0500

    Initial Commit

---
+++ PaperRockScissorsChoose/PaperRockScissorsChooseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperRockScissorsChooseApp.swift
+//  PaperRockScissorsChoose
+//
+//  Created by Danny Tsang on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct PaperRockScissorsChooseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
